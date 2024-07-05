6c0d3aca5d1a8aa328614bd95a0639d1a8069e66
grinderos <grinderos.coding@gmail.com>
2024-07-05 20:52:43 +0300

    fix

--- src/main/java/academy/kata/mis/personservice/util/PersonServiceManager.java
+++ src/main/java/academy/kata/mis/personservice/util/PersonServiceManager.java
@@ -1,6 +1,5 @@
 package academy.kata.mis.personservice.util;
 
-import academy.kata.mis.personservice.service.PersonBusinessService;
 import academy.kata.mis.personservice.service.PersonService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -9,13 +8,8 @@ import org.springframework.stereotype.Service;
 @AllArgsConstructor
 public class PersonServiceManager {
     private final PersonService personService;
-    private final PersonBusinessService personBusinessService;
 
     public PersonService getPersonService() {
         return personService;
-    }
-
-    public PersonBusinessService getPersonBusinessService() {
-        return personBusinessService;
     }
 }
